96020c485e41741c26a7ee262712eb77e2d9ddd7
Klemense <klemenseloh@gmail.com>
2022-01-31 00:00:04 -0500

    commit tree 840606119da1b33e1bef985c9cda0648ccaf4e15

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 10;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()*10)+1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,20);
